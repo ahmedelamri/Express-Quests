1cf9ef6ed0f3c2d6b1b4c7b87539a18e8be4bd24
AHMED EL AMRI <ahmed-elamri@outlook.fr>
2023-05-08 18:52:55 +0200

    express02

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
+require("dotenv").config();
 const express = require("express");
-
 const app = express();
-
-const port = 5000;
+const port = process.env.APP_PORT ?? 5036;
 
 const welcome = (req, res) => {
-  res.send("Welcome to my favourite movie list");
+  res.send(
+    "Welcome to my favourite movie list in french | Bienvenue sur ma liste de films préférés "
+  );
 };
 
 app.get("/", welcome);
@@ -14,6 +15,8 @@ const movieHandlers = require("./movieHandlers");
 
 app.get("/api/movies", movieHandlers.getMovies);
 app.get("/api/movies/:id", movieHandlers.getMovieById);
+app.get("/api/users", movieHandlers.getUsers);
+app.get("/api/users/:id", movieHandlers.getUsersById);
 
 app.listen(port, (err) => {
   if (err) {
